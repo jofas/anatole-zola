45f2652eba8dcef35648694314822dcada0bf2c9
jofas <jonas@fc-web.de>
2023-07-22 18:15:19 +0200

    admonition shortcode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,21 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      'admonition-note-light': '#fdfdfe',
+      'admonition-note-dark': '#474748',
+      'admonition-note-border': '#d4d5d8',
       'admonition-tip-light': '#e6f6e6',
-      'admonition-tip-border-light': '#009400',
       'admonition-tip-dark': '#003100',
-      'admonition-tip-border-dark': '#009400',
+      'admonition-tip-border': '#009400',
+      'admonition-info-light': '#eef9fd',
+      'admonition-info-dark': '#193c47',
+      'admonition-info-border': '#4cb3d4',
+      'admonition-caution-light': '#fff8e6',
+      'admonition-caution-dark': '#4d3800',
+      'admonition-caution-border': '#e6a700',
+      'admonition-danger-light': '#ffebec',
+      'admonition-danger-dark': '#4b1113',
+      'admonition-danger-border': '#e13238',
     }
   },
   plugins: [],
