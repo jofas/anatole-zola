18a3240c87d554a63e55a39ae92e1aa0f3f2f484
jofas <jonas@fc-web.de>
2023-07-22 17:53:19 +0200

    started working on admonition shortcode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["templates/**/*.html"],
+  content: ['templates/**/*.html'],
   theme: {
     extend: {},
+    colors: {
+      'admonition-tip-light': '#e6f6e6',
+      'admonition-tip-border-light': '#009400',
+      'admonition-tip-dark': '#003100',
+      'admonition-tip-border-dark': '#009400',
+    }
   },
   plugins: [],
   darkMode: 'class',
